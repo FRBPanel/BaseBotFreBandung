a6bedf3173ca1b0c164b486677b8032cb9647984
FRBPanel <infotransaksifrb@gmail.com>
2025-07-30 17:33:21 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+global.usePairingCode = true; // Set to true if you want to use pairing code, false for normal login
+global.prefa = ['', '!', '.', '#', '$', ',']
+
+global.botName = 'FRBot by FreDev'
+global.ownerName = 'FreDev'
+global.ownerNumber = '628123456789' // Replace with your WhatsApp number
